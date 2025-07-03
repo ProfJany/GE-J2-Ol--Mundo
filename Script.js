a51540db550813d51943195a2654fed7e8b1b542
Janice Cordeiro <133601287+ProfJany@users.noreply.github.com>
2025-07-03 19:12:39 -0300

    1

--- Script.js
+++ Script.js
@@ -2,16 +2,11 @@ function inserirNome(){
  let nomeUsuario = prompt("Qual o seu nome");
  let elemento = document.querySelector("#nome-usuario");
  elemento.textContent = nomeUsuario;
+ }
+
+ inserirNome();
+
  let linguagens = ["JavaScript","Python","C"];​
-console.log(linguagens[0]) // “JavaScript”
-console.log(linguagens[1]) // “Python”
-console.log(linguagens[2]) // “C”
-const item = document.querySelector("#lista");
-item.textContent = linguagens[0];
-item.textContent = linguagens[1];
-item.textContent = linguagens[2];
-const linguagens = ["JavaScript","Python","C"];​
-linguagens[2] = "HTML"; //ERRO
-let linguagens = ["JavaScript","Python","C"];​
-linguagens[2] = "HTML"; // ["JavaScript","Python","HTML"]
-}
+console.log(linguagens[0]);
+console.log(linguagens[1]);
+console.log(linguagens[2]);
